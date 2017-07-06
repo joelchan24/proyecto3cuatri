90d4d6217969c7b503b6d06ed71c7356b688fc7e
ArmandoCanul <armando.canul@outlook.com>
2017-07-06 03:51:51 -0500

    tarea

---
+++ BO/HorarioBO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BO
+{
+    class HorarioBO
+    {
+        public int Codigo { get; set; }
+        public TimeSpan HoraInicio { get; set; }
+        public TimeSpan HoraFin { get; set; }
+        public string Lunes { get; set; }
+        public string Martes { get; set; }
+        public string Miercoles { get; set; }
+        public string Jueves { get; set; }
+        public string Viernes { get; set; }
+        public string Sabado { get; set; }
+        public string Domingo { get; set; }
+    }
+}
---
+++ BO/UsuarioBO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BO
+{
+    public class UsuarioBO
+    {
+        public int Codigo { get; set; }
+        public string Nombre { get; set; }
+        public string Apellidos { get; set; }
+        public string Telefono { get; set; }
+        public DateTime FechaNacimineto { get; set; }
+        public string Correo { get; set; }
+        public string Usuario { get; set; }
+        public string Contraseña { get; set; }
+        public string Fotografia { get; set; }
+        public int Codigo_Direccion { get; set; }
+
+    }
+}
