e243bf4e4f9901b0dba28184283f72b6dd464f4a
Gerardo Uicab <electro.gospel277gmail.com>
2017-07-18 13:23:33 -0500

    tarea

--- AdministradorGUI/Usuario.aspx.cs
+++ AdministradorGUI/Usuario.aspx.cs
@@ -14,6 +14,7 @@ namespace AdministradorGUI
     {
         usuarioDAO objusuario = new usuarioDAO();
         UsuarioBO objbo = new UsuarioBO();
+        ctrol_usuarioSERVICIOS ctrl = new ctrol_usuarioSERVICIOS();
         protected void Page_Load(object sender, EventArgs e)
         {
             if (!IsPostBack)
@@ -36,7 +37,31 @@ namespace AdministradorGUI
 
         public UsuarioBO recolectar()
         {
+            UsuarioBO objbo = new UsuarioBO();
+            int id = 0; int.TryParse(txtID.Text, out id);
+            objbo.Codigo = id;
+            objbo.Nombre = txtNombre.Text;
+            objbo.Apellidos = txtApellido.Text;
+            objbo.FechaNacimineto = Convert.ToDateTime(txtfecha.Text);
+            objbo.Telefono = txtTelefono.Text;
+            objbo.Correo = txtCorreo.Text;
+            objbo.IDCUENTA = Convert.ToInt32(ddlTipousuario.SelectedValue);
+            objbo.Usuario = txtUsuario.Text;
+            objbo.Contraseña = txtContraseña.Text;
+            objbo.CodigoMunicipio = Convert.ToInt32(ddlMunicipio.SelectedValue);
+            objbo.Colonia = txtColonia.Text;
+            objbo.CodigoPostal = txtCodigoPostal.Text;
+            objbo.Cruzamiento = txtCruzamiento.Text;
+            objbo.NumeroInterior = txtNumeroInterior.Text;
+            objbo.NumeroExterior = txtNumeroExteriror.Text;
+            return objbo;
 
+        }
+
+        protected void accion1(object sender, EventArgs e)
+        {
+            Button obj = (Button)sender;
+            ctrl.accion1(recolectar(), obj.ID);
         }
     }
 }
