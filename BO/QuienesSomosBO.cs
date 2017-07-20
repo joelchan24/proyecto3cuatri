40080b46515604242b2d1efddf7c9a5a7127a088
RODRIGO\Anahi <Rodrigomena622@gmail.com>
2017-07-20 10:22:09 -0500

    Clases quienes somos

---
+++ BO/QuienesSomosBO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BO
+{
+    public class QuienesSomosBO
+    {
+        public int id_Quienes { get; set; }
+        public string descripcion { get; set; }
+    }
+}
