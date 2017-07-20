417468bbd969a4f05666de9ec79b7e53d4170149
Gerardo Uicab <electro.gospel277gmail.com>
2017-07-19 20:56:02 -0500

    ey

--- AdministradorGUI/Usuario.aspx.designer.cs
+++ AdministradorGUI/Usuario.aspx.designer.cs
@@ -362,5 +362,14 @@ namespace AdministradorGUI {
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
         protected global::System.Web.UI.WebControls.GridView dtgDatos;
+        
+        /// <summary>
+        /// Control txtDir.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HiddenField txtDir;
     }
 }
