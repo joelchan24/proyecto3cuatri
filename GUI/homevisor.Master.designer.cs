67c08c8721faeec7bd183d7748782f4a34e6bc9e
ArmandoCanul <armando.canul@outlook.com>
2017-07-27 10:07:42 -0600

    yisus

--- GUI/homevisor.Master.designer.cs
+++ GUI/homevisor.Master.designer.cs
@@ -58,6 +58,15 @@ namespace GUI {
         protected global::System.Web.UI.WebControls.Button btn_cerrar2;
         
         /// <summary>
+        /// Button2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button2;
+        
+        /// <summary>
         /// txt_usuaer1 control.
         /// </summary>
         /// <remarks>
