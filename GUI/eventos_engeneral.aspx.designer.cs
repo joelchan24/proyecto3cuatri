ba16288b778f84a77cba40761e9ecd2f91f511da
Joel Chan <joelchan1220@gmail.com>
2017-07-27 15:24:41 -0500

    evento

--- GUI/eventos_engeneral.aspx.designer.cs
+++ GUI/eventos_engeneral.aspx.designer.cs
@@ -13,6 +13,24 @@ namespace GUI {
     public partial class eventos_engeneral {
         
         /// <summary>
+        /// Control txt_buscar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txt_buscar;
+        
+        /// <summary>
+        /// Control btn.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.LinkButton btn;
+        
+        /// <summary>
         /// Control list.
         /// </summary>
         /// <remarks>
